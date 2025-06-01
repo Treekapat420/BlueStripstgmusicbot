c40db2c8424b9307db8043da6b531041fbe4aa70
Treekapat420 <pcoltsfan23@aol.com>
2025-06-01 00:18:34 -0400

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -56,6 +56,9 @@ class Telegram(Client):
         await call.add_bot(self)
         await call.register_decorators()
         await super().start()
+        assistant = await self.get_me()
+self.logger.info(f"Assistant User ID: {assistant.id}")
+self.logger.info(f"Assistant Username: @{assistant.usernames.editable_username if assistant.usernames else 'N/A'}")
         await self.call_manager.start_scheduler()
 
         self.logger.info(
